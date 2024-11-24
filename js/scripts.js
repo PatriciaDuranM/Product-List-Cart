2114915142c907b4872a1513362a6e9ec7d31291
PatriciaDuranM <patduran16@gmail.com>
2024-11-24 22:38:01 +0100

    update

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const filterElement = document.getElementById('filter-tab');
+
+const activateFilter = () =>{
+    const filter = event.target.dataset.filter{
+        if(!filter) return;
+
+        
+    }
+
+
+}
+
+
+
+filterElement.addEventListener('click', activateFilter);
